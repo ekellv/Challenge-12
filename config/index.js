bb2bac472c795f7da98ce29254b055c9c34a3ccb
Erin V <erinkvoelker@gmail.com>
2022-08-15 20:36:19 -0400

    setting up connection between mysql databases and user prompts

--- config/index.js
+++ config/index.js
@@ -8,12 +8,61 @@ class employeeDatabase {
         this.connection = connection;
     }
 
-    allEmployees() {
+    viewDepts() {
         return this.connection.promise().query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+            `SELECT department.id, department.name FROM department`;
         );
     }
 
+    addDepts(department) {
+        return this.connection.promise().query(
+            `INSERT INTO department SET ?, department`;
+        );
+    }
 
+    viewRoles() {
+        return this.connection.promise().query(
+            `SELECT role.id. 
+            role.title, 
+            department.name AS department FROM role, 
+            role.salary FROM role, 
+            INNER JOIN department on role.department_id = department.id;`
+        );
+    }
 
+    addRole() {
+        return this.connection.promise().query(
+            `INSERT INTO role SET ?`, role
+        );
+    }
+
+    viewEmployees() {
+        return this.connection.promise().query(
+            `SELECT employee.id, 
+            employee.first_name, 
+            employee.last_name, 
+            role.title, 
+            department.name AS department, 
+            role.salary, 
+            CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee 
+            LEFT JOIN role on employee.role_id = role.id 
+            LEFT JOIN department on role.department_id = department.id 
+            LEFT JOIN employee manager on manager.id = employee.manager_id`;
+        );
+    }
+
+    addEmployee() {
+        return this.connection.promise().query(
+            `INSERT INTO employee SET ?`, employee 
+        );
+    }
+
+    updateEmpRole() {
+        return this.connection.promise().query(
+            `UPDATE employee SET role_id = ? WHERE id = ?`, 
+            [roleID, employeeID]
+        );
+    }
 }
+
+module.exports = new employeeDatabase(connection);
